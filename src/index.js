c9fdc414a31d6362d3f564711dad4aa5ece43a34
Artur Kleshchanka <try3987@gmail.com>
2020-10-21 20:40:29 +0300

    chore: write brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    
+    var st = []
+    var br = Object.fromEntries (bracketsConfig)
+    var tm = str.split (``)
+    
+    for (var i in tm) {
+        if (st[0] === tm[i]) {
+            st.shift()
+        }
+        else if (br[tm[i]]) {
+            st.unshift (br[tm[i]])
+        }
+        else {
+            return false
+        }
+    }
+    return st.length === 0
 }
